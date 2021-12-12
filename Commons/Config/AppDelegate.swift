83c6e0a06c4196c0649f3570d37061815f887c4d
Carlos Mejía <carlosmejia083@gmail.com>
2021-12-12 10:52:09 -0500

    Initial project creation.

---
+++ Commons/Config/AppDelegate.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+@main class AppDelegate: UIResponder {
+    // MARK: - Properties
+    var window: UIWindow?
+    
+    // MARK: - Private Methods
+    private func setupNavigator() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = LoginViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
+// MARK: - UIResponder, UIApplicationDelegate
+extension AppDelegate: UIApplicationDelegate  {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        setupNavigator()
+        return true
+    }
+}
---
+++ Scenes/Create/CreateViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
